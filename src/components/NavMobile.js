c31bdbbd7c88939112d3a4db48987ad34f22b78f
Byte Store <aham161999@gmail.com>
2022-09-15 18:33:00 +0200

    first commit

---
+++ src/components/NavMobile.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+// import menu btn
+import { BiMenu } from "react-icons/bi";
+
+const NavMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav>
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className="cursor-pointer text-4xl text-heading ml-[10px] lg:hidden"
+      >
+        <BiMenu />
+      </div>
+      <ul
+        className={`${
+          isOpen ? "max-h-60 p-8" : "max-h-0 p-0"
+        } flex flex-col absolute w-full top-24 left-0 bg-white rounded-md shadow-primary space-y-6 overflow-hidden transition-all`}
+      >
+        <li>
+          <a href="#" onClick={() => setIsOpen(!isOpen)}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={() => setIsOpen(!isOpen)}>
+            Classes
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={() => setIsOpen(!isOpen)}>
+            Features
+          </a>
+        </li>
+        <li>
+          <a href="#" onClick={() => setIsOpen(!isOpen)}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavMobile;
